9ad58e4c4da3e9a33a2d5ab6a833d051514d4e5d
Balamurugan Janakiraman <balamurugan.janakiraman@syniverse.com>
2025-02-12 11:22:53 +0530

    initial code commit

---
+++ src/test/java/com/pujitha/playground/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pujitha.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
